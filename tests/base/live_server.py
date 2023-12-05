b66b26bff841cc9eb6b0169c00c66f1ff0df3c7f
emurze <grayemurze@gmail.com>
2023-12-05 23:27:55 +0300

    remove(staging_server env var) live_server.py and feature(localhost) drive.pyr

--- tests/base/live_server.py
+++ tests/base/live_server.py
@@ -1,12 +1,9 @@
-import os
 import socket
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class MyLiveServerTestCase(StaticLiveServerTestCase):
-    host = os.getenv(
-        'STAGING_SERVER', socket.gethostbyname(socket.gethostname())
-    )
+    host = socket.gethostbyname(socket.gethostname())
     print('\n' * 6, host, '\n' * 6)
     port = 8081
