deb0236f72c9a7f521f2b2139d3d445803c1a935
emurze <grayemurze@gmail.com>
2023-12-05 23:23:53 +0300

    feature(staging_server env var) live_server.py

--- tests/base/live_server.py
+++ tests/base/live_server.py
@@ -8,4 +8,5 @@ class MyLiveServerTestCase(StaticLiveServerTestCase):
     host = os.getenv(
         'STAGING_SERVER', socket.gethostbyname(socket.gethostname())
     )
+    print('\n' * 6, host, '\n' * 6)
     port = 8081
