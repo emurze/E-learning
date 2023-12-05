53e2590bda34f8c2efad0a317815a7b9d5bebde0
emurze <grayemurze@gmail.com>
2023-12-05 23:41:28 +0300

    removing(print host) live_server.py

--- tests/base/live_server.py
+++ tests/base/live_server.py
@@ -5,5 +5,4 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class MyLiveServerTestCase(StaticLiveServerTestCase):
     host = socket.gethostbyname(socket.gethostname())
-    print('\n' * 6, host, '\n' * 6)
     port = 8081
